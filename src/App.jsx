3bb454126127a2a0083053102d8d75327ed669b2
pranavp-work <pranavp.work@gmail.com>
2024-12-14 15:18:36 +0530

    bootstrap fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import { Routes, Route } from 'react-router-dom';
 import UserHome from './../pages/user/UserHome';
 import UserHeader from './../components/user/UserHeader';
 import UserFooter from './../components/user/UserFooter';
+
 
 function App() {
 
